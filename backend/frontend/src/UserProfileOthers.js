8d3f6e3fbe16ca10fa3593af98d84a03d023056c
ckertam <ckertam1996@gmail.com>
2023-04-21 02:40:48 +0300

    Other user's profile page and follow function #34

--- backend/frontend/src/UserProfileOthers.js
+++ backend/frontend/src/UserProfileOthers.js
@@ -6,43 +6,79 @@ import './UserProfile.css';
 const UserProfileOthers = () => {
   const [user, setUser] = useState(null);
   const [profilePhotoUrl, setProfilePhotoUrl] = useState(null);
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followerCount, setFollowerCount] = useState(0);
   const { id } = useParams();
 
-  useEffect(() => {
-    fetchUserDetails();
-    fetchProfilePhoto(); 
-  }, []);
-
-  const fetchUserDetails = async () => {
+  const getCurrentUser = async () => {
     try {
-      const response = await axios.get(`http://localhost:8000/user/userDetails/${id}`, {
-        headers: {
-        },
+      const response = await axios.get('http://localhost:8000/user/user', {
+        headers: {},
         withCredentials: true,
       });
-      setUser(response.data);
+      return response.data;
     } catch (error) {
-      console.error('Error fetching user details:', error);
+      console.error('Error fetching current user:', error);
+      return null;
     }
   };
 
-  const fetchProfilePhoto = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8000/user/profilePhoto/${id}`, {
-        headers: {
-        },
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userDetailsResponse = await axios.get(`http://localhost:8000/user/userDetails/${id}`, {
+          headers: {},
+          withCredentials: true,
+        });
+        setUser(userDetailsResponse.data);
+
+        const profilePhotoResponse = await axios.get(`http://localhost:8000/user/profilePhoto/${id}`, {
+          headers: {},
+          withCredentials: true,
+          responseType: 'arraybuffer',
+        });
+        const base64Image = Buffer.from(profilePhotoResponse.data, 'binary').toString('base64');
+
+        const contentType = profilePhotoResponse.headers['content-type'];
+        const dataUrlPrefix = contentType === 'image/jpeg' ? 'data:image/jpeg;base64,' : 'data:image/png;base64,';
+
+        setProfilePhotoUrl(`${dataUrlPrefix}${base64Image}`);
+        
+        const followersResponse = await axios.get(`http://localhost:8000/user/userFollowers/${id}`, {
+        headers: {},
         withCredentials: true,
-        responseType: 'arraybuffer',
+        });
+        const currentUser = await getCurrentUser();
+
+        const isCurrentUserFollowing = followersResponse.data.some(
+            (follower) => follower.id === currentUser
+        );
+        setIsFollowing(isCurrentUserFollowing);
+
+        setFollowerCount(userDetailsResponse.data.followers.length);
+      } catch (error) {
+        console.error('Error fetching user details and profile photo:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleFollowClick = async () => {
+    try {
+      const response = await axios.post(`http://localhost:8000/user/followByUser/${id}`, null, {
+        headers: {},
+        withCredentials: true,
       });
-      const base64Image = Buffer.from(response.data, 'binary').toString('base64');
-  
-      // Determine the data URL prefix based on the file extension
-      const contentType = response.headers['content-type'];
-      const dataUrlPrefix = contentType === 'image/jpeg' ? 'data:image/jpeg;base64,' : 'data:image/png;base64,';
-  
-      setProfilePhotoUrl(`${dataUrlPrefix}${base64Image}`);
+      if (response.data === "followed") {
+        setIsFollowing(true);
+        setFollowerCount((prevCount) => prevCount + 1);
+      } else if (response.data === "unfollowed") {
+        setIsFollowing(false);
+        setFollowerCount((prevCount) => prevCount - 1);
+      }
     } catch (error) {
-      console.error('Error fetching profile photo:', error);
+      console.error('Error following/unfollowing user:', error);
     }
   };
 
@@ -55,16 +91,18 @@ const UserProfileOthers = () => {
       <h1>{user.username}'s Profile</h1>
       {profilePhotoUrl && (
         <img
-            src={profilePhotoUrl}
-            alt={`${user.username}'s profile`}
-            className="profile-photo" 
+          src={profilePhotoUrl}
+          alt={`${user.username}'s profile`}
+          className="profile-photo"
         />
-        )}
+      )}
       {/* <p>ID: {user.id}</p>
       <p>Email: {user.email}</p> */}
       <p>Biography: {user.biography}</p>
-      <p>Followers: {user.followers.length}</p>
-      
+      <p>Followers: {followerCount}</p>
+      <button onClick={handleFollowClick}>
+        {isFollowing ? 'Unfollow' : 'Follow'}
+      </button>
     </div>
   );
 };
