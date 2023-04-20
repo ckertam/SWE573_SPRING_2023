41010842999a2c3aab163094de02c29aab95a821
ckertam <ckertam1996@gmail.com>
2023-04-21 02:39:00 +0300

    Homepage stories with pagination frontend #33

--- backend/frontend/src/StoriesbyFollowingUsers.js
+++ backend/frontend/src/StoriesbyFollowingUsers.js
@@ -7,14 +7,23 @@ function StoriesByFollowingsUsers() {
   const [stories, setStories] = useState([]);
   const [usernames, setUsernames] = useState({});
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasPrevPage, setHasPrevPage] = useState(false);
+  const [hasNextPage, setHasNextPage] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const storiesResponse = await axios.get('http://localhost:8000/user/userStories', { withCredentials: true });
-        setStories(storiesResponse.data);
-        const authorIds = storiesResponse.data.map(story => story.author);
+        const storiesResponse = await axios.get(`http://localhost:8000/user/userStories?page=${currentPage}&size=10`, { withCredentials: true });
+        setStories(storiesResponse.data.stories);
+        setHasNextPage(storiesResponse.data.has_next);
+        setHasPrevPage(storiesResponse.data.has_prev);
+        setTotalPages(storiesResponse.data.total_pages);
+
+        const authorIds = storiesResponse.data.stories.map(story => story.author);
         if (authorIds.length > 0) {
           const usernamesPromises = authorIds.map(id =>
             axios.get(`http://localhost:8000/user/usernamesbyId?user_ids[]=${id}`)
@@ -33,11 +42,17 @@ function StoriesByFollowingsUsers() {
       }
     };
     fetchData();
-  }, []);
+  }, [currentPage]);
 
   const handleStoryClick = async (id) => {
     navigate(`/story/${id}`);
   }
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
 
   return (
     <div>
@@ -51,6 +66,23 @@ function StoriesByFollowingsUsers() {
           </div>
         ))
       )}
+      <div className={styles.pagination}>
+        <button onClick={() => handlePageChange(currentPage - 1)} disabled={!hasPrevPage}>
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            className={index + 1 === currentPage ? styles.active : null}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button onClick={() => handlePageChange(currentPage + 1)} disabled={!hasNextPage}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
